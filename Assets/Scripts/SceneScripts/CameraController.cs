192348fc0811de984fea424fa4740603fbe884db
BrianAnco <ancobrian6@gmail.com>
2022-04-23 09:38:43 -0500

    Movimiento de Cámara

--- Assets/Scripts/SceneScripts/CameraController.cs
+++ Assets/Scripts/SceneScripts/CameraController.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform player;
+    public Transform activeRoom;
+
+    [Range(-5, 5)]
+    public float minModX, maxModX, minModY, maxModY;
+    
+
+
+    public static CameraController instance;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+            instance = this;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,6 +32,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var minPosY = activeRoom.GetComponent<BoxCollider2D>().bounds.min.y;
+        var maxPosY = activeRoom.GetComponent<BoxCollider2D>().bounds.max.y;
+        var minPosX = activeRoom.GetComponent<BoxCollider2D>().bounds.min.x + minModX;
+        var maxPosX = activeRoom.GetComponent<BoxCollider2D>().bounds.max.x + maxModX;
+
+        Vector3 clampedPos = new Vector3(
+            Mathf.Clamp(player.position.x, minPosX, maxPosX),
+            Mathf.Clamp(player.position.y, minPosY, maxPosY),
+            Mathf.Clamp(player.position.z, -10f, -10f)
+            );
+
+        transform.position = new Vector3(clampedPos.x, clampedPos.y, clampedPos.y);
     }
 }
